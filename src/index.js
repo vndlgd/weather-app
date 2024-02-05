b614f377919c4c533d1295f900ebc0d720bec7da
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-02-05 00:32:45 -0800

    Implement form for user input to fetch weather info using the weather API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import './style.css';
+import './style.css'; // Import the CSS file
 
-// Let's code!
+const searchBar = document.querySelector('#search-bar');
+const searchButton = document.querySelector('#search-button');
+const locationHeader = document.querySelector('#location');
+
+searchButton.addEventListener('click', fetchLocation);
+
+// create async function
+async function fetchLocation() {
+  try {
+    // prepare searchValue for api
+    let searchValue = searchBar.value.replace(' ', '-').toLowerCase();
+    const response = await fetch(
+      'http://api.weatherapi.com/v1/current.json?key=e0b3e243fbb04b31a0891012240202&q=' +
+        searchValue
+    );
+    const locationData = await response.json();
+    if (locationData.location.country === 'United States of America') {
+      // truncate USA string to be centered inside div
+      locationData.location.country = locationData.location.country.substring(
+        0,
+        13
+      );
+    }
+    // locationHeader.textContent = `${locationData.location.name} - ${locationData.location.country}`;
+    console.log(locationData);
+  } catch (err) {
+    console.log(err);
+  }
+}
